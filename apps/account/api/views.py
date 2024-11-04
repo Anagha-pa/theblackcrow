ec3fa47cda74653cff38907b6eb7a38509b276e4
Anagha <anagha.pa@prosevo.com>
2024-11-04 23:52:14 +0530

    fd

--- apps/account/api/views.py
+++ apps/account/api/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import *
 
 # Create your views here.
+
+
 
 
 class UsersLoginView(APIView):
@@ -15,4 +17,14 @@ class UsersLoginView(APIView):
         if serializer.is_valid():
             return Response(serializer.validated_data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
- 
+
+
+
+# view for registering users
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
